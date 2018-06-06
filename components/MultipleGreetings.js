ea6f8170620b0e143b40f925495804a97a1981fa
joylty94 <wowmen1757@gmail.com>
2018-06-06 20:43:46 +0900

    props 사용

---
+++ components/MultipleGreetings.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+class Greeting extends Component{
+    render(){
+        let greetingString = `
+        Hello ${ this.props.name }, How are you ?
+        `
+        return(
+            <Text>{ greetingString }</Text>
+        )
+    }
+}
+
+export default class MultipleGreetings extends Component{
+    render(){
+        return(
+            <View style= {{ alignItems: "center" }}>
+                <Greeting name="Lee"></Greeting>
+                <Greeting name="Tae Yong"></Greeting>
+            </View>
+        )
+    }
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 // import App from './App';
-import Hello from './components/Hello'
+// import Hello from './components/Hello'
+import MultipleGreetings from './components/MultipleGreetings'
 
-AppRegistry.registerComponent('native_tutorial', () => Hello);
+AppRegistry.registerComponent('native_tutorial', () => MultipleGreetings);
