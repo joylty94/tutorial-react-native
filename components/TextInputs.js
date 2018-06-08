247e6a2910887c518176e66280ba24051ffe1556
joylty94 <wowmen1757@gmail.com>
2018-06-08 17:18:02 +0900

    keyboard 사용

--- components/TextInputs.js
+++ components/TextInputs.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, Keyboard } from "react-native";
 
 export default class TextInputs extends Component {
     constructor(props) {
         super(props);
         this.state = {
             typedText: 'please type your Text',
-            typedPassword: ''
+            typedPassword: '',
+            typedDescription: '',
         };
+    }
+    componentWillMount () {
+        this.keyboardDidshowListener = Keyboard.addListener('keyboardDidshow', ()=>{
+            this.setState(() => {
+                return { typedText: 'keyboard is shown'}
+            })
+        })
+        this.keyboardDidshowListener = Keyboard.addListener('keyboardDidHide', ()=>{
+            this.setState(() => {
+                return { typedText: 'keyboard Hide'}
+            })
+        })
+    }
+    componentWillUnmount () {
+        this.keyboardDidshowListener.remove()
     }
     render() {
         return (
@@ -46,12 +62,39 @@ export default class TextInputs extends Component {
                     placeholder="Enter your password"
                     placeholderTextColor="gray"
                     secureTextEntry={true}
+                    // autoFocus={true}
                     onChangeText={
                         (text) => {
                             this.setState(
                                 (previousState) => {
                                     return {
                                         typedPassword: text
+                                    };
+                                }
+                            );
+                        }
+                    }
+                />
+                <TextInput 
+                    style= {{
+                        height: 200,
+                        margin: 20,
+                        padding: 10,
+                        borderColer: 'gray',
+                        borderWidth: 1
+                    }}
+                    multiline={true}
+                    borderBottomColer='green'
+                    borderBottomWithd={3}
+                    returnKeyType='none'
+                    editable={true}
+                    onSubmitEditing={Keyboard.dismiss}
+                    onChangeText={
+                        (text) => {
+                            this.setState(
+                                (previousState) => {
+                                    return {
+                                        typedDescription: text
                                     };
                                 }
                             );
