9221de698f27248f45087b146208e5fbc0017fd8
joylty94 <wowmen1757@gmail.com>
2018-07-22 15:26:32 +0900

    imgURL 추가

--- data/flatListData.js
+++ data/flatListData.js
@@ -3,26 +3,31 @@ var flatListData = [
         "key" : "key1",
         "name" : "Lee Tea Yong",
         "age" : "26",
+        "imgURL" : 'https://cdn.namuwikiusercontent.com/s/3764b48c237ee811aa77a7d57679f7fbf4d1bde0492d910886fc76e56ea13379f35c37d7b992648b41816bb48910f4f7bd2459309da0c1ad1dd0b3f5e42f372ebc6d9861cfb758b2317ce04e381fcef5?e=1536692150&k=OTYgIa_E9o7h9tbsJ7660Q'
     },
     {
         "key" : "key2",
         "name" : "Lee",
         "age" : "2",
+        "imgURL": 'https://cdn.namuwikiusercontent.com/s/3764b48c237ee811aa77a7d57679f7fbf4d1bde0492d910886fc76e56ea13379f35c37d7b992648b41816bb48910f4f7bd2459309da0c1ad1dd0b3f5e42f372ebc6d9861cfb758b2317ce04e381fcef5?e=1536692150&k=OTYgIa_E9o7h9tbsJ7660Q'
     },
     {
         "key" : "key3",
         "name" : "Lee Tea",
         "age" : "28",
+        "imgURL": 'https://cdn.namuwikiusercontent.com/s/3764b48c237ee811aa77a7d57679f7fbf4d1bde0492d910886fc76e56ea13379f35c37d7b992648b41816bb48910f4f7bd2459309da0c1ad1dd0b3f5e42f372ebc6d9861cfb758b2317ce04e381fcef5?e=1536692150&k=OTYgIa_E9o7h9tbsJ7660Q'
     },
     {
         "key" : "key4",
         "name" : "Yong",
         "age" : "26",
+        "imgURL": 'https://cdn.namuwikiusercontent.com/s/3764b48c237ee811aa77a7d57679f7fbf4d1bde0492d910886fc76e56ea13379f35c37d7b992648b41816bb48910f4f7bd2459309da0c1ad1dd0b3f5e42f372ebc6d9861cfb758b2317ce04e381fcef5?e=1536692150&k=OTYgIa_E9o7h9tbsJ7660Q'
     },
     {
         "key" : "key1",
         "name" : "Tea",
         "age" : "20",
+        "imgURL": 'https://cdn.namuwikiusercontent.com/s/3764b48c237ee811aa77a7d57679f7fbf4d1bde0492d910886fc76e56ea13379f35c37d7b992648b41816bb48910f4f7bd2459309da0c1ad1dd0b3f5e42f372ebc6d9861cfb758b2317ce04e381fcef5?e=1536692150&k=OTYgIa_E9o7h9tbsJ7660Q'
     },
 ]
 
