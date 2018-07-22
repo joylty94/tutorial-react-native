c45edd14a580c10503a5c041c18c5881a19309ca
joylty94 <wowmen1757@gmail.com>
2018-07-22 14:32:09 +0900

    FlatList data

---
+++ data/flatListData.js
@@ -0,0 +1,29 @@
+var flatListData = [
+    {
+        "key" : "key1",
+        "name" : "Lee Tea Yong",
+        "age" : "26",
+    },
+    {
+        "key" : "key2",
+        "name" : "Lee",
+        "age" : "2",
+    },
+    {
+        "key" : "key3",
+        "name" : "Lee Tea",
+        "age" : "28",
+    },
+    {
+        "key" : "key4",
+        "name" : "Yong",
+        "age" : "26",
+    },
+    {
+        "key" : "key1",
+        "name" : "Tea",
+        "age" : "20",
+    },
+]
+
+module.exports = flatListData;
